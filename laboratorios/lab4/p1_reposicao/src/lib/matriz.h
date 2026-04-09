4a2beabe8642798b0519729e183bfbdf6e80574f
Dimitry Deveza <dimydeveza@gmail.com>
2026-04-09 19:50:43 -0300

    feito p1_reposicao exercicio 4

---
+++ laboratorios/lab4/p1_reposicao/src/lib/matriz.h
@@ -0,0 +1,13 @@
+#ifndef _MATRIZ_H
+#define _MATRIZ_H
+
+typedef struct matriz tMatriz;
+
+tMatriz *criaMatriz(int linha, int coluna);
+void setElemento(tMatriz *m, int linha, int coluna, char *elemento);
+char* getElemento(tMatriz *m, int linha, int coluna);
+void exterminaMatriz(tMatriz *m);
+void organizaMatriz(tMatriz *base);
+void imprimeMatriz(tMatriz *m);
+
+#endif
---
+++ laboratorios/lab4/p1_reposicao/src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lib/matriz.h"
+
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		perror("[erro] Foram passados menos argumentos do que o necessário.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (freopen(argv[1], "r", stdin) == NULL) {
+		perror("[erro] freopen falhou ao abrir o arquivo de input");
+		exit(EXIT_FAILURE);
+	}
+
+	if (freopen(argv[2], "w", stdout) == NULL) {
+		perror("[erro] freopen falhou ao abrir o arquivo de output");
+		exit(EXIT_FAILURE);
+	}
+
+	int linhas, colunas;
+  char elemento[1024];
+
+	scanf("%d %d", &linhas, &colunas);
+	tMatriz *base = criaMatriz(linhas, colunas);
+
+	for (int i = 0; i < linhas; i++) {
+		for (int j = 0; j < colunas; j++) {
+			scanf("%s", elemento);
+
+			setElemento(base, i, j, elemento);
+		}
+	}
+
+	imprimeMatriz(base);
+  printf("============================\n");
+	organizaMatriz(base);
+  imprimeMatriz(base);
+  exterminaMatriz(base);
+
+	exit(EXIT_SUCCESS);
+}
